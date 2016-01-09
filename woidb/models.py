1707f23fb9302c930b8a0d873631629d17c81cf4
Aaron Toth <atoth89@gmail.com>
2016-01-08 21:20:48 -0500

    Added Player table schema. Small misc schema changes

--- woidb/models.py
+++ woidb/models.py
@@ -6,6 +6,21 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
+class Player(Base):
+    __tablename__ = 'players'
+    id = Column(String, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    position = Column(String(length=2))
+    shoots = Column(String(length=1))
+    birth_date = Column(Date)
+    height = Column(Integer)
+    weight = Column(Integer)
+
+    def __repr__(self):
+        return '<Player(name=\'{}, {}\' id={})>'.format(self.last_name, self.first_name, self.id)
+
+
 class Team(Base):
     __tablename__ = 'teams'
     id = Column(String, primary_key=True)
@@ -15,14 +30,17 @@ class Team(Base):
     city = Column(String)
     games = relationship('Game')
 
+    def __repr__(self):
+        return '<Team(id={})>'.format(self.teamname)
+
 
 class Game(Base):
     __tablename__ = 'games'
-    id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
     season = Column(String)
     status = Column(Integer)
     awayteam = Column(String, ForeignKey('teams.id'))
-    hometeam = Column(String)
+    hometeam = Column(String, ForeignKey('teams.id'))
     awayscore = Column(Integer)
     homescore = Column(Integer)
     date = Column(Date)
@@ -35,5 +53,8 @@ class Game(Base):
     awayafterhome = Column(Boolean)
     homeafteraway = Column(Boolean)
     homeafterhome = Column(Boolean)
+
+    def __repr__(self):
+        return '<Game(away={}, home={}, date={})>'.format(self.awayteam, self.hometeam, self.date)
 
 
